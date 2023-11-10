d714b9235a61410731ab729b8248deea61e424cb
amanullahjn99 <amanullahjn99@gmail.com>
2023-11-10 17:42:09 -0500

    i have added the feature lights

--- src/test/java/main.java
+++ src/test/java/main.java
@@ -1,9 +1,15 @@
-public class main {
+public class
+main {
     public static void main(String[] args) {
         CarFeatures audi=new CarFeatures();
         audi.color="black";
         audi.Hp="5000";
         audi.Doors="2";
         audi.Model="A90";
+
+        Lights light=new Lights();
+        light.color="blue";
+        light.opacity="50";
+        light.size="40";
     }
 }
